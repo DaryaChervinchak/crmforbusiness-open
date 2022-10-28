c9965bcc2ed73f858f75f9b119f50fb2fa755ade
Darya <daria.chervinchak@masterdata.ru>
2022-10-28 20:58:00 +0300

    28.10.2022 20:57

--- js/main.js
+++ js/main.js
@@ -1,202 +1,211 @@
 $(function () {
-  $(".products__slider ").slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.png" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.png" alt=""></img>',
-    dots: true,
-    responsive: [{
-        breakpoint: 1369,
-        settings: {
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 1100,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 882,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          centerMode: true,
-          arrows: false,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 540,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          arrows: false,
-          dots: false,
-        },
-      },
-    ],
-  });
+	$(".products__slider ").slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.png" alt=""></img>',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.png" alt=""></img>',
+		dots: true,
+		responsive: [{
+				breakpoint: 1369,
+				settings: {
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 882,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+					arrows: false,
+					dots: false,
+				},
+			},
+			{
+				breakpoint: 540,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: true,
+					arrows: false,
+					dots: false,
+				},
+			},
+		],
+	});
 
-  $(".advantages__slider ").slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.png" alt=""></img>',
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.png" alt=""></img>',
-    dots: true,
-    responsive: [{
-        breakpoint: 1369,
-        settings: {
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 882,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          centerMode: true,
-          arrows: false,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 540,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          arrows: false,
-          dots: false,
-        },
-      },
-    ],
-  });
-  $(".awards__items").slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    arrows: false,
-    responsive: [{
-        breakpoint: 1150,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 652,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-        },
-      },
-    ],
-  });
+	$(".advantages__slider ").slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.png" alt=""></img>',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.png" alt=""></img>',
+		dots: true,
+		responsive: [{
+				breakpoint: 1369,
+				settings: {
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 882,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+					arrows: false,
+					dots: false,
+				},
+			},
+			{
+				breakpoint: 540,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: true,
+					arrows: false,
+					dots: false,
+				},
+			},
+		],
+	});
+	$(".awards__items").slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dots: true,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1150,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 652,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: true,
+					dots: false,
+				},
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: false,
+				},
+			},
+		],
+	});
 
-  $(".confidence__slider").slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplaySpeed: 800,
-    dots: false,
-    arrows: false,
-    responsive: [{
-        breakpoint: 1100,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 882,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 540,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
+	$(".confidence__slider").slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplaySpeed: 800,
+		dots: false,
+		arrows: false,
+		responsive: [{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				breakpoint: 882,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 540,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
 
-  const anchors = document.querySelectorAll("a.jsScroll");
+	$(".icon-menu").on("click", function (e) {
+		$("body").toggleClass("menu-open");
+	});
 
-  for (let anchor of anchors) {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
+	const anchors = document.querySelectorAll("a.jsScroll");
 
-      const blockID = anchor.getAttribute("href");
+	for (let anchor of anchors) {
+		anchor.addEventListener("click", function (e) {
+			e.preventDefault();
 
-      document.querySelector(blockID).scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    });
-  }
+			if ($("body").hasClass("menu-open")) {
+				$("body").removeClass("menu-open");
+			}
 
-  $(".event__btn").click(function (event) {
-    event.preventDefault();
+			const blockID = anchor.getAttribute("href");
 
-    let dataID = $(this).attr("data-id");
-    let dataTitle = $(this).attr("data-title");
-    let dataText = $(this).attr("data-text");
+			document.querySelector(blockID).scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			});
 
-    $(".modal input[name=Campaign_ID]").val(dataID);
-    $(".modal .form__title").text(dataTitle);
-    $(".modal .form__text").text(dataText);
-    $(".modal, .modal__overlay ").fadeIn(400);
-  });
+		});
+	}
 
-  $(".modal__close, .modal__overlay").click(function () {
-    $(".modal, .modal__overlay").fadeOut(400);
-  });
+	$(".event__btn").on("click", function (event) {
+		event.preventDefault();
 
-  $(".jsForm").on("submit", function (e) {
-    e.preventDefault();
-    const _this = $(this);
-    _this.ajaxSubmit({
-      crossDomain: true,
-      dataType: "jsonp",
-      _this: this,
-      complete: function () {
-        _this.hide();
-        _this.next().removeClass("hidden");
-      },
-    });
-  });
+		let dataID = $(this).attr("data-id");
+		let dataTitle = $(this).attr("data-title");
+		let dataText = $(this).attr("data-text");
 
-  $(document).ready(function () {
-    $('input[name="oid"]').each(function (index, input) {
-      input.value = "00D200000008P1X";
-    });
-  });
+		$(".modal input[name=Campaign_ID]").val(dataID);
+		$(".modal .form__title").text(dataTitle);
+		$(".modal .form__text").text(dataText);
+		$(".modal, .modal__overlay ").fadeIn(400);
+	})
+
+	$(".modal__close, .modal__overlay").on("click", function (e) {
+		$(".modal, .modal__overlay").fadeOut(400);
+	});
+
+	$(".jsForm").on("submit", function (e) {
+		e.preventDefault();
+		const _this = $(this);
+		_this.ajaxSubmit({
+			crossDomain: true,
+			dataType: "jsonp",
+			_this: this,
+			complete: function () {
+				_this.hide();
+				_this.next().removeClass("hidden");
+			},
+		});
+	});
+
+	$(document).ready(function () {
+		$('input[name="oid"]').each(function (index, input) {
+			input.value = "00D200000008P1X";
+		});
+	});
 });
